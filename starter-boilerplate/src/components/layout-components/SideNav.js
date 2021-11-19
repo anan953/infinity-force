b09c93d268edc36cdbf7f260c3a0e81fe1eead1a
harryHoang <anhv3773@gmail.com>
2021-11-19 12:06:49 +0700

    update Menu sidebar

--- starter-boilerplate/src/components/layout-components/SideNav.js
+++ starter-boilerplate/src/components/layout-components/SideNav.js
@@ -1,33 +1,36 @@
 import React from "react";
-import { Layout } from 'antd';
-import { connect } from 'react-redux';
-import { SIDE_NAV_WIDTH, SIDE_NAV_DARK, NAV_TYPE_SIDE } from 'constants/ThemeConstant';
-import { Scrollbars } from 'react-custom-scrollbars';
-import MenuContent from './MenuContent'
+import { Layout } from "antd";
+import { connect } from "react-redux";
+import { SIDE_NAV_WIDTH, SIDE_NAV_DARK, NAV_TYPE_SIDE } from "constants/ThemeConstant";
+import { Scrollbars } from "react-custom-scrollbars";
+import styled from "styled-components";
+import MenuContent from "./MenuContent";
+
+const WrapperLogo = styled.div`
+  padding: 45px 16px;
+`;
+
+const Logo = styled.img``;
 
 const { Sider } = Layout;
 
-export const SideNav = ({navCollapsed, sideNavTheme, routeInfo, hideGroupTitle, localization = true }) => {
-  const props = { sideNavTheme, routeInfo , hideGroupTitle, localization}
+export const SideNav = ({ navCollapsed, sideNavTheme, routeInfo, hideGroupTitle, localization = true }) => {
+  const props = { sideNavTheme, routeInfo, hideGroupTitle, localization };
   return (
-    <Sider 
-      className={`side-nav ${sideNavTheme === SIDE_NAV_DARK? 'side-nav-dark' : ''}`} 
-      width={SIDE_NAV_WIDTH} 
-      collapsed={navCollapsed}
-    >
+    <Sider className={`side-nav ${sideNavTheme === SIDE_NAV_DARK ? "side-nav-dark" : ""}`} width={SIDE_NAV_WIDTH} collapsed={navCollapsed}>
+      <WrapperLogo>
+        <Logo alt="logo" src="/img/logo.png" />
+      </WrapperLogo>
       <Scrollbars autoHide>
-        <MenuContent 
-          type={NAV_TYPE_SIDE} 
-          {...props}
-        />
+        <MenuContent type={NAV_TYPE_SIDE} {...props} />
       </Scrollbars>
     </Sider>
-  )
-}
+  );
+};
 
 const mapStateToProps = ({ theme }) => {
-  const { navCollapsed, sideNavTheme } =  theme;
-  return { navCollapsed, sideNavTheme }
+  const { navCollapsed, sideNavTheme } = theme;
+  return { navCollapsed, sideNavTheme };
 };
 
 export default connect(mapStateToProps)(SideNav);
--- starter-boilerplate/src/configs/NavigationConfig.js
+++ starter-boilerplate/src/configs/NavigationConfig.js
@@ -1,11 +1,21 @@
 import { DashboardOutlined } from "@ant-design/icons";
 import { APP_PREFIX_PATH } from "configs/AppConfig";
+import CustomIcon from "components/util-components/CustomIcon";
+import styled from "styled-components";
 
 const dashBoardNavTree = [
   {
     key: "dashboard",
     path: `${APP_PREFIX_PATH}/dashboard`,
     title: "dashboard",
+    icon: DashboardOutlined,
+    breadcrumb: false,
+    submenu: [],
+  },
+  {
+    key: "payment",
+    path: `${APP_PREFIX_PATH}/payment`,
+    title: "Payments",
     icon: DashboardOutlined,
     breadcrumb: false,
     submenu: [],
