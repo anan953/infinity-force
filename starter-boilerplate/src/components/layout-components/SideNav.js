c6b8f65dce2d142697ef578a5167c8f0dbc90354
harryHoang <anhv3773@gmail.com>
2021-11-20 14:27:01 +0700

    update build app

--- starter-boilerplate/src/components/layout-components/SideNav.js
+++ starter-boilerplate/src/components/layout-components/SideNav.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { Layout } from "antd";
 import { connect } from "react-redux";
-import {
-  SIDE_NAV_WIDTH,
-  SIDE_NAV_DARK,
-  NAV_TYPE_SIDE,
-} from "constants/ThemeConstant";
+import { SIDE_NAV_WIDTH, SIDE_NAV_DARK, NAV_TYPE_SIDE } from "constants/ThemeConstant";
 import { Scrollbars } from "react-custom-scrollbars";
 import styled from "styled-components";
 import MenuContent from "./MenuContent";
@@ -18,24 +14,12 @@ const Logo = styled.img``;
 
 const { Sider } = Layout;
 
-export const SideNav = ({
-  navCollapsed,
-  sideNavTheme,
-  routeInfo,
-  hideGroupTitle,
-  localization = true,
-}) => {
+export const SideNav = ({ navCollapsed, sideNavTheme, routeInfo, hideGroupTitle, localization = true }) => {
   const props = { sideNavTheme, routeInfo, hideGroupTitle, localization };
   return (
-    <Sider
-      className={`side-nav ${
-        sideNavTheme === SIDE_NAV_DARK ? "side-nav-dark" : ""
-      }`}
-      width={SIDE_NAV_WIDTH}
-      collapsed={navCollapsed}
-    >
+    <Sider className={`side-nav ${sideNavTheme === SIDE_NAV_DARK ? "side-nav-dark" : ""}`} width={SIDE_NAV_WIDTH} collapsed={navCollapsed}>
       <WrapperLogo>
-        <Logo alt="logo" src="/img/logo.png" />
+        <Logo alt="logo" src="/infinity-force/img/logo.png" />
       </WrapperLogo>
       <Scrollbars autoHide>
         <MenuContent type={NAV_TYPE_SIDE} {...props} />
